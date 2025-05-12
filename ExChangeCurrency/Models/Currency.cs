989c8eac13f7fd4317b6f42a6a8e3f352ad4f961
aestdile@gmail.com <aestdile@gmail.com>
2025-05-12 10:24:30 +0500

    Add project files.

---
+++ ExChangeCurrency/Models/Currency.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCource.Models
+{
+    public class Currency
+    {
+        public int id { get; set; }
+        public string Ccy { get; set; }
+        public string CcyNm_UZ { get; set; }
+        public string Rate { get; set; }
+    }
+}
---
+++ ExChangeCurrency/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace CurrencyCource.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // done
+        public string FirstName { get; set; } // done
+        public string LastName { get; set; } // done
+        public int Age { get; set; } // done
+        public DateTime DateOfBirth { get; set; } // done
+        public string Email { get; set; } // done
+        public string PhoneNumber { get; set; } // done 
+        public string Password { get; set; } // done
+        public decimal Balance { get; set; } // done
+    }
+}
